8b2a3d6f128386cd37d479a1cc1c0a16b5422345
Andrew Plaza <andrew.plaza@parity.io>
2020-08-30 23:09:04 +0200

    fix sha256

--- vim-bbye/derivation.nix
+++ vim-bbye/derivation.nix
@@ -7,11 +7,11 @@ vimUtils.buildVimPlugin {
     owner = "moll";
     repo = "vim-bbye";
     rev = "25ef93ac5a87526111f43e5110675032dbcacf56";
-    sha256 = "1111111111111111111111111111111111111111111111111111";
+    sha256 = "0dlifpbd05fcgndpkgb31ww8p90pwdbizmgkkq00qkmvzm1ik4y4";
   };
 
   meta = with stdenv.lib; {
-    description = "Insipx's Penrose WM";
+    description = "Delete buffers and close files in Vim without closing your windows or messing up your layout";
     homepage = https://github.com/moll/vim-bbye;
     license = licenses.agpl3;
     maintainers = [ "dev@andrewplaza.dev" ];
