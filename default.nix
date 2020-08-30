c705c9e06196c5b878f79a3f69fbbbe9d77a84a2
Andrew Plaza <andrew.plaza@parity.io>
2020-08-30 17:49:58 +0200

    first commit

---
+++ default.nix
@@ -0,0 +1,3 @@
+self: super: {
+  penrose = self.callPackage ./penrose/derivation.nix {};
+}
---
+++ penrose/derivation.nix
@@ -0,0 +1,28 @@
+{ stdenv, fetchFromGitHub, rustPlatform, pkgconfig
+, cairo, pango, python3, libX11, libxcb
+, xcbutilkeysyms, xcbutil, xmodmap, libxkbcommon }:
+
+rustPlatform.buildRustPackage rec {
+  name = "penrose";
+  version = "0.1.0";
+  src = fetchFromGitHub {
+    owner = "insipx";
+    repo = "penrose-insipx";
+    rev = "9744466edded099c2b7ac27a5b6344e7f476629d";
+    sha256 = "0b0y7a03gq2h9gqm5l1xk5y8vyh4gyvkq49myb499l5rd0sw32yl";
+  };
+
+  buildInputs = [ pkgconfig cairo pango python3 libX11 libxcb 
+                  xcbutilkeysyms xcbutil xmodmap libxkbcommon ];
+  checkPhase = "";
+  cargoSha256 = "sha256:1w92sab57jrylnpccrc4j6if34i3lkprxnbwlp2qmfc4j2llj574";
+  verifyCargoDeps = true;
+
+  meta = with stdenv.lib; {
+    description = "Insipx's Penrose WM";
+    homepage = https://github.com/insipx/penrose-insipx;
+    license = licenses.mit;
+    maintainers = [ "dev@andrewplaza.dev" ];
+    platforms = platforms.linux;
+  };
+}
