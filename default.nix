7c6078748aa1bd9380444b1bc63a156f32b4c878
Andrew Plaza <andrew.plaza@parity.io>
2021-06-07 18:34:45 -0400

    remove stdenv

--- default.nix
+++
@@ -1,5 +0,0 @@
-self: super: {
-  penrose = self.callPackage ./penrose/derivation.nix {};
-  vim-bbye = self.callPackage ./vim-bbye/derivation.nix {};
-  rust-analyzer-wrapped = self.callPacker ./rust-analyzer-wrapped/derivation.nix {};
-}
